f347977623b1771c1a080f8db5c6dbbd9db3dab2
Moses Martins <mmoses061@gmail.com>
2025-07-31 11:56:36 +0100

    update structure

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import(
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	
+	router := mux.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + port,
+	}
+
+	log.Printf("Serving on: http://localhost:%s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
